970c2ce7839ecac1aeffd3f7a41be28952fc5345
Mikael Håkansson <wmmihaa@hotmail.com>
2019-09-03 21:52:22 +0200

    Updated unit tests

--- test/scenario1.js
+++ test/scenario1.js
@@ -9,6 +9,7 @@ var assert = require('assert');
 var request = require('supertest');
 var should = require('should');
 var fs = require('fs');
+require('colors');
 var SCRIPTFOLDER = path.resolve(process.env.HOME, "microServiceBus/services");
 var util;
 var MicroServiceBusHost;
@@ -20,13 +21,16 @@ var signedIn = false;
 var settings;
 var loggedInComplete1;
 var microServiceBusHost;
+var flowResult;
+var ttlCollection;
+var persistHelper;
 
 describe('Util functions', function () {
 
     it('Prepare settings', function (done) {
         util = require("../lib/utils.js");
         SettingsHelper = require("./SettingsHelper.js");
-        // util.prepareNpm(new SettingsHelper());
+        util.prepareNpm(new SettingsHelper());
         // require('app-module-path').addPath(process.env.NODE_PATH);
         // require('module').globalPaths.push(process.env.NODE_PATH);
         // require('module')._initPaths();
@@ -68,7 +72,94 @@ describe('Util functions', function () {
         expect(r).to.equal(1);
         done();
     });
+    it('compress should work', function (done) {
+        var srcPath = path.resolve(__dirname,'../README.md' );
+        var dstPath = path.resolve(__dirname,'../README.md' );
+        util.compress (srcPath, dstPath, function(err, fileName){
+            should.not.exist(err);
+            done();
+        });       
+    });
+    it('getDependanciesRecursive should work', function (done) {
+        this.timeout(6000);
+        util.getDependanciesRecursive(function(err, fileName){
+            should.not.exist(err);
+            done();
+        });
+       
+    });
+    it('getDependanciesRecursive should work', function (done) {
+        this.timeout(6000);
+        util.getAvailableDiskspaceRaw(function(err, fileName){
+            should.not.exist(err);
+            done();
+        });
+       
+    });
+    
+});
 
+describe('TTL functions', function (){
+    it('Init TTLCollection should work', function (done) {
+        let TTLCollection = require("../lib/TTLCollection");
+        const TTLNAME = "TTLSAMPLE"
+        const TTLHISTORY_TTL = 7 * 24 * 60 * 60 * 1000; // one week
+        const TTLHISTORY_CHECKINTERVAL = 5 * 60 * 1000; // every 5 minutes
+        const TTLHISTORY_PERSISTINTERVAL = 5 * 60 * 1000; // 5 minutes
+        
+        ttlCollection = new TTLCollection({
+            key: TTLNAME,
+            ttl: TTLHISTORY_TTL,
+            checkPeriod: TTLHISTORY_CHECKINTERVAL,      // Interval to check for expired items
+            persistPeriod: TTLHISTORY_PERSISTINTERVAL,  // Interval (this.options.persistPeriod) for persising self._collection
+            persistDir: path.resolve(__dirname, "../coverage"),
+            persistFileName: TTLNAME + '.json'
+        });
+        try{
+            ttlCollection.push(false);
+            done();
+        }
+        catch(err){
+            should.not.exist(err);
+        }
+        
+    });
+    it('filterCollection should work', function (done) {
+        this.timeout(6000);
+        ttlCollection.filterCollection(Date.parse('2019-09-01'), new Date(), 'hour', function (err, filteredCollection) {
+            should.not.exist(err);
+            expect(filteredCollection).to.have.lengthOf(1);
+            done();
+        });
+    });
+    it('pushUnique should work', function (done) {
+        this.timeout(6000);
+        
+        try{
+            ttlCollection.pushUnique(42, 42, "agroup");
+            done();
+        }
+        catch(err){
+            should.not.exist(err);
+        }
+
+        ttlCollection.filterCollection(Date.parse('2019-09-01'), new Date(), 'hour', function (err, filteredCollection) {
+            should.not.exist(err);
+            expect(filteredCollection).to.have.lengthOf(2);
+            done();
+        });
+    });
+    it('filterByGroup  should work', function (done) {
+        this.timeout(6000);
+        try{
+            let group = ttlCollection.filterByGroup ("agroup");
+            expect(group).to.have.lengthOf(1);
+            done();
+        }
+        catch(err){
+            should.not.exist(err);
+        }
+    });
 });
 
 describe('Encryption/Decryption', function () {
@@ -110,8 +201,8 @@ describe('Check configuration', function () {
         done();
     });
 });
-return;
-describe('Sign in', function () {
+
+describe('Run scenario test', function () {
     it('Save settings should work', function (done) {
         SettingsHelper = require("./SettingsHelper.js");
         settingsHelper = new SettingsHelper();
@@ -132,7 +223,14 @@ describe('Sign in', function () {
         settingsHelper.save();
         done();
     });
+    it('saveSettings should work', function (done) {
+        util.saveSettings(settingsHelper.settings, function(err){
+            should.not.exist(err);
+            done();
+        });
+    });
     it('Create microServiceBus Node should work', function (done) {
+        this.timeout(60000);
         try {
             loggedInComplete1 = false;
             MicroServiceBusHost = require("../lib/MicroServiceBusHost.js");
@@ -156,10 +254,6 @@ describe('Sign in', function () {
                 done();
 
             }
-
-        });
-        microServiceBusHost.OnStopped(function () {
-
         });
         try {
             microServiceBusHost.Start();
@@ -169,7 +263,23 @@ describe('Sign in', function () {
             expect(er).to.be.null;
             done();
         }
-
+    });
+    it('Flow should complete', function (done) {
+        this.timeout(10000);
+        microServiceBusHost.OnUnitTestComplete(function (result) {
+            expect(1).to.eql(1);
+            flowResult = result;
+            done();
+        });
+    });
+    it('Flow result should be good', function (done) {
+        for(let property in flowResult){
+            expect(flowResult[property]).to.eql(true);
+            let result = flowResult[property] ? "√".green : "failed".red  
+            console.log('\t' +property + ' : ' + result);
+        }
+        
+        done();
     });
     it('Enable tracking should work', function (done) {
         this.timeout(60000);
@@ -185,13 +295,6 @@ describe('Sign in', function () {
             done();
         });
     });
-    // it('Upload syslogs should work', function (done) {
-    //     this.timeout(60000);
-    //     var r = microServiceBusHost.TestOnUploadSyslogs(function (sucess) {
-    //         expect(sucess).to.equal(true);
-    //         done();
-    //     });
-    // });
     it('Ping should work', function (done) {
         this.timeout(60000);
         var r = microServiceBusHost.TestOnPing("test");
@@ -208,6 +311,32 @@ describe('Sign in', function () {
     });
 });
 
+describe('PersistHelper functions', function (){
+    it('Init PersistHelper should work', function (done) {
+        let PersistHelper = require("../lib/PersistHelper");
+
+        try{
+            persistHelper = new PersistHelper(settingsHelper);
+            done();
+        }
+        catch(err){
+            should.not.exist(err);
+        }
+        
+    });
+    it('persist should work', function (done) {
+        this.timeout(6000);
+        persistHelper.persist({id:1}, 'event', function (err) {
+            should.not.exist(err);
+          });
+    });
+    it('there should be persisted messages', function (done) {
+        this.timeout(6000);
+        expect(persistHelper.storage.keys()).to.have.lengthOf(1);
+    });
+    
+});
+
 describe('Post Signin', function () {
     it('azureApiAppInboundService.js should exist after login', function (done) {
         var filePath = path.resolve(SCRIPTFOLDER, "azureApiAppInboundService.js");
@@ -216,63 +345,7 @@ describe('Post Signin', function () {
 
         done();
     });
-    it('calling test should work', function (done) {
-        this.timeout(5000);
-        var url = 'http://localhost:9090';
 
-        request(url)
-            .get('/api/data/azureApiAppInboundService1/test')
-            .expect('Content-Type', 'application/json; charset=utf-8')
-            .expect(200)//Status code
-            .end(function (err, res) {
-                if (err) {
-                    throw err;
-                }
-                res.body.should.have.property('result');
-                res.body.result.should.equal(true);
-                console.log("GET Complete");
-                //done();
-                request(url)
-                    .delete('/api/data/azureApiAppInboundService1/test')
-                    .expect('Content-Type', 'application/json; charset=utf-8')
-                    .expect(200)//Status code
-                    .end(function (err, res) {
-                        if (err) {
-                            throw err;
-                        }
-                        res.body.should.have.property('result');
-                        res.body.result.should.equal(true);
-                        console.log("DELETE Complete");
-                        request(url)
-                            .post('/api/data/azureApiAppInboundService1/test')
-                            .send({ name: 'Manny', species: 'cat' })
-                            .expect('Content-Type', /json/)
-                            .expect(200)//Status code
-                            .end(function (err, res) {
-                                if (err) {
-                                    throw err;
-                                }
-                                res.body.should.have.property('result');
-                                res.body.result.should.equal(true);
-                                console.log("POST Complete");
-                                request(url)
-                                    .put('/api/data/azureApiAppInboundService1/test')
-                                    .send({ name: 'Manny', species: 'cat' })
-                                    .expect('Content-Type', /json/)
-                                    .expect(200)//Status code
-                                    .end(function (err, res) {
-                                        if (err) {
-                                            throw err;
-                                        }
-                                        res.body.should.have.property('result');
-                                        res.body.result.should.equal(true);
-                                        console.log("PUT Complete");
-                                        done();
-                                    });
-                            });
-                    });
-            });
-    });
     it('javascriptaction.js should exist after calling service', function (done) {
         var filePath = path.resolve(__dirname, SCRIPTFOLDER, "javascriptaction.js");
 
@@ -290,10 +363,18 @@ describe('Post Signin', function () {
         var TestOnChangeDebugResponse = microServiceBusHost.TestOnChangeState("Stop");
         done();
     });
-    it('removeNpmPackage (msbcam) should work', function (done) {
-        this.timeout(30000);
-        util.removeNpmPackage("msbcam", function (err) {
-            expect(err).to.be.null;
+    // it('removeNpmPackage (msbcam) should work', function (done) {
+    //     this.timeout(30000);
+    //     util.removeNpmPackage("msbcam", function (err) {
+    //         expect(err).to.be.null;
+    //         done();
+    //     });
+    // });
+    it('Stop should work', function (done) {
+        this.timeout(10000);
+        
+        microServiceBusHost.TestStop(function(err){
+            should.not.exist(err);
             done();
         });
     });
